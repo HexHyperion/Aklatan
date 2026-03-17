177ca992aed3dbad82330ca690483cfd9f0e7660
HexHyperion <szymonjan@gmail.com>
2026-03-17 11:25:09 +0100

    Implement auth with session, read .env file, add routes, extend README

--- AklatanServer/build.gradle.kts
+++ AklatanServer/build.gradle.kts
@@ -1,9 +1,11 @@
 val kotlin_version: String by project
 val logback_version: String by project
+val ktor_version: String by project
 
 plugins {
     kotlin("jvm") version "2.3.0"
     id("io.ktor.plugin") version "3.4.0"
+    kotlin("plugin.serialization") version "2.3.0"
 }
 
 group = "com.hexhyperion"
@@ -23,6 +25,12 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-sessions:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.0")
+    implementation("io.ktor:ktor-server-content-negotiation:3.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
